01c568d9bba2cbb044a84cb16ef209d028c91c61
janarthanan <janarthanan@cgvakindia.com>
2017-08-29 15:59:52 +0530

    Add project files.

---
+++ Practice1/Migrations/20170828132541_MyFirstProgram.Designer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Practice1.Models;
+
+namespace Practice1.Migrations
+{
+    [DbContext(typeof(ApplicationContext))]
+    [Migration("20170828132541_MyFirstProgram")]
+    partial class MyFirstProgram
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Practice1.Models.Book", b =>
+                {
+                    b.Property<int>("Id");
+
+                    b.Property<string>("AuthorName")
+                        .IsRequired()
+                        .HasMaxLength(100);
+
+                    b.Property<string>("BookTitle")
+                        .IsRequired()
+                        .HasMaxLength(50);
+
+                    b.Property<string>("Genre");
+
+                    b.Property<int>("Price");
+
+                    b.Property<string>("Publisher")
+                        .IsRequired()
+                        .HasMaxLength(100);
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Books");
+                });
+        }
+    }
+}
---
+++ Practice1/Models/AppContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+namespace Practice1.Models
+{
+    public class ApplicationContext :DbContext
+    {
+        public ApplicationContext(DbContextOptions opts): base(opts)
+        {
+        }
+        public DbSet< Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+    }
+}
